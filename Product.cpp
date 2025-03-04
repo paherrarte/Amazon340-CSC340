5e4cc07b7dd285ef05596590952d97a1b58c6d8b
Omeid Nadery <omeid.n@icloud.com>
2025-03-03 18:03:26 -0800

    product built

--- Product.cpp
+++ Product.cpp
@@ -1,8 +1,25 @@
 // TO DO: #include all the standard libraries and your own libraries here
-
 // TO DO: function implementations
-
+#include "Product.h"
 // ------------------------------------------------------------------------------
+Product::Product(string name, string description, double rating){
+	this->name = name; 
+	this->description = description;
+	this->rating = rating;
+	this->timesSold = 0;
+}
+
+//getters
+string Product::getName() const {return name;}
+string Product::getDescription() const {return description;}
+double Product::getRating() const {return rating;}
+int Product::getTimesSold() const {return timesSold;}
+
+//setters
+void Product::setName(string newName) {name = newName;}
+void Product::setDescription(string newDescription) {description = newDescription;}
+void Product::setRating(double newRating) {rating = newRating;}
+
 // Operator overloading implementation
 bool Product::operator==(const Product& otherProduct) const {
 	return Product::name == otherProduct.name;
--- Product.h
+++ Product.h
@@ -1,10 +1,44 @@
 // TO DO: #include all the standard libraries and your own libraries here
-
+#include <iostream>
+#include <string>
 // To DO: define the class Product with the necessary functions' prototypes and data fields
-
+#ifndef PRODUCT_H
+#define PRODUCT_H
 // This is a function that allows you to compare two products based on their name. 
 // You may directly include it in your class definition. 
 // You don't need to modify it but will have to put it inside your class. 
 // Operator overloading function prototype:
-bool operator==(const Product& otherProduct) const; 
+using namespace std;
+
+class Product{
+    string name;
+    string description;
+    double rating; 
+    int timesSold;
+
+    public:
+    Product(string name, string description, double rating);
+
+    virtual ~Product(){}
+
+    //Getters
+    string getName() const;
+    string getDescription() const;
+    double getRating() const;
+    int getTimesSold() const;
+
+    //setters
+    void setName(string newName);
+    void setDescription(string newDescription);
+    void setRating(double newRating);
+
+    //sell function
+    virtual void sell() = 0;
+
+    //operator overload
+    bool operator==(const Product& otherProduct) const; 
+
+};
+
+#endif 
 
